71621013c74011b2f99b1db328300ff47298802c
Moritz Höwer <Moritz.Hoewer@haw-hamburg.de>
2016-09-28 22:30:38 +0200

    Added headers and changed .gitignore

--- src/aufgabenblatt01/wiederholung/Student.java
+++ src/aufgabenblatt01/wiederholung/Student.java
@@ -1,3 +1,11 @@
+/*
+ * Praktikum Programmiermethodik 2 - WS 2016
+ * Gruppe:  Philip Scheer (Philip.Scheeer@haw-hamburg.de),
+ * 	        Moritz Höwer (Moritz.Hoewer@haw-hamburg.de)
+ * 
+ * Datum: 28.09.2016 
+ * Aufgabe: Aufgabenblatt 1 Aufgabe 1
+ */
 package aufgabenblatt01.wiederholung;
 
 import java.util.LinkedList;
